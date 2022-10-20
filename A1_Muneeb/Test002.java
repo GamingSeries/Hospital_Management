c338910892ce0f404d1350c231ef51c624375bf2
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2022-10-20 11:10:45 +0400

    Update Test002.java

--- A1_Muneeb/Test002.java
+++ A1_Muneeb/Test002.java
@@ -1,5 +1,12 @@
-public class Test002 {
-    public static void main(String[] args) {
+public class Test002 
+{
+    public static void main(String[] args)
+    {
+        Appointment copy_app1 = new Appointment();
+        Appointment copy_app2 = new Appointment();
+        
+        copy_app1 = copy_app2;
+        Date copy_date1 = new Date(13, 4, 2022);
         
     }
 }
