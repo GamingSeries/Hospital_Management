811641861ff6203e542077332ce7e738b1808e6e
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2022-11-20 04:01:08 +0400

    minors

--- A2_Muneeb/src/Patient.java
+++ A2_Muneeb/src/Patient.java
@@ -1,28 +1,29 @@
-public class Patient implements Comparable<Patient>{
+public class Patient implements Comparable<Patient> {
 	private String patientID;
 	private Date date;
 	private String problem;
 	private int priority;
-	
-	
+
 	/**
-	 * @param patientID adds PatientID  
-	 * @param date adds date
-	 * @param problem adds problem
-	 * @param priority adds priority
+	 * @param patientID adds PatientID
+	 * @param date      adds date
+	 * @param problem   adds problem
+	 * @param priority  adds priority
 	 */
-	Patient(String patientID, Date date,String problem,int priority){
-		this.patientID=patientID;
-		this.date=date;
-		this.problem=problem;
-		this.priority=priority;
+	Patient(String patientID, Date date, String problem, int priority) {
+		this.patientID = patientID;
+		this.date = date;
+		this.problem = problem;
+		this.priority = priority;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
-	public String toString(){
-		return patientID+" "+date+" "+problem+" "+priority;
+	public String toString() {
+		return patientID + " " + date + " " + problem + " " + priority;
 	}
 
 	/**
@@ -53,21 +54,21 @@ public class Patient implements Comparable<Patient>{
 		return priority;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Comparable#compareTo(java.lang.Object)
 	 */
 	@Override
 	public int compareTo(Patient o) {
-	
-		if(priority==o.priority){
+
+		if (priority == o.priority) {
 			return 0;
-		}
-		else if(priority<o.priority){
+		} else if (priority < o.priority) {
 			return 1;
-		}
-		else{
+		} else {
 			return -1;
 		}
 	}
-	
+
 }
